409ea69b3cff7f89539598847c412ebc903329f4
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-29 15:30:14 +0330

    create searchCoins

--- Crypto-App/src/components/templates/HomePage.jsx
+++ Crypto-App/src/components/templates/HomePage.jsx
@@ -4,11 +4,13 @@ import { useEffect } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinsList } from "../../services/cryptoApi";
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
 
   useEffect(() => {
     // fetch(getCoinsList())
@@ -16,15 +18,20 @@ function HomePage() {
     //   .then((json) => setCoins(json));
     setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinsList(page));
-      const json = await res.json();
-      setCoins(json);
-      setIsLoading(false);
+      try {
+        const res = await fetch(getCoinsList(page, currency));
+        const json = await res.json();
+        setCoins(json);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
     getData();
-  }, [page]);
+  }, [page, currency]);
   return (
     <div>
+      <Search currency={currency} setCurrency={setCurrency} />
       <TableCoin coins={coins} isLoading={isLoading} />
       <Pagination page={page} setPage={setPage} />
     </div>

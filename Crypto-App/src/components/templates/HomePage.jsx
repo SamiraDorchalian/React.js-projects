11946f7eb44387a9a6da47dc02750cbbcf4240bf
samira dorchalian <samiradorchalain1998@gmail.com>
2024-10-29 13:45:20 +0330

    create Pagination

--- Crypto-App/src/components/templates/HomePage.jsx
+++ Crypto-App/src/components/templates/HomePage.jsx
@@ -3,26 +3,30 @@ import { useEffect } from "react";
 
 import TableCoin from "../modules/TableCoin";
 import { getCoinsList } from "../../services/cryptoApi";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     // fetch(getCoinsList())
     //   .then((res) => res.json())
     //   .then((json) => setCoins(json));
+    setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinsList());
+      const res = await fetch(getCoinsList(page));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
     getData();
-  }, []);
+  }, [page]);
   return (
     <div>
       <TableCoin coins={coins} isLoading={isLoading} />
+      <Pagination page={page} setPage={setPage} />
     </div>
   );
 }

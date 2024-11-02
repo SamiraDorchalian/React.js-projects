d248d3205eb7f5439b7cc9e605be3bedb00c5aa5
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-02 11:24:37 +0330

    create context for products

---
+++ store-react-project/src/context/ProductsProvider.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+import api from "./../services/config";
+
+const ProductsContext = createContext();
+
+function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={products}>
+      {children}
+    </ProductsContext.Provider>
+  );
+}
+
+export default ProductsProvider;

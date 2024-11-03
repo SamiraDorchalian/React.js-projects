1bab08ce79084b1249f40a8a26994a073d9ddeeb
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-03 16:43:30 +0330

    create emptypage

--- store-react-project/src/components/EmptyPage.jsx
+++ store-react-project/src/components/EmptyPage.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import image from "../images/empty.png";
+
+import styles from "./EmptyPage.module.css";
 
 function EmptyPage() {
-  return <div>EmptyPage</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <p>Your Basket is Empty</p>
+      </div>
+      <div>
+        <img src={image} alt="image" />
+      </div>
+    </div>
+  );
 }
 
 export default EmptyPage;

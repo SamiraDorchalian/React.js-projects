44c448de4192533dfd7f7fce9447947df20afdea
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-08 12:13:04 +0330

    create authors part

--- graphql-materialul/src/component/author/Authors.js
+++ graphql-materialul/src/component/author/Authors.js
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { GET_AUTHORS_INFO } from "../../graphql/Queries";
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
 
 function Authors() {
   const { loading, data, error } = useQuery(GET_AUTHORS_INFO);
@@ -9,7 +10,37 @@ function Authors() {
   if (loading) return <h4>Loading...</h4>;
   if (error) return <h4>somethinf went wrong</h4>;
 
-  return <div>Authors</div>;
+  return (
+    <Grid
+      container
+      sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}
+    >
+      {data.authors.map((author, index) => (
+        <React.Fragment key={author.id}>
+          <Grid item xs={12} padding={2}>
+            <a
+              href={`/authors/${author.slug}`}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                textDecoration: "none",
+              }}
+            >
+              <Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />
+              <Typography component="p" variant="p" color="text.secondary">
+                {author.name}
+              </Typography>
+            </a>
+          </Grid>
+          {index !== data.authors.length - 1 && (
+            <Grid item xs={12}>
+              <Divider variant="middle" />
+            </Grid>
+          )}
+        </React.Fragment>
+      ))}
+    </Grid>
+  );
 }
 
 export default Authors;

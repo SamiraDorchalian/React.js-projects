67ad864cecdf43fe55d6f3c9dc8ee3cee2aa6e71
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-08 14:32:03 +0330

    get query for post

--- graphql-materialul/src/component/blog/BlogPage.js
+++ graphql-materialul/src/component/blog/BlogPage.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { GET_POST_INFO } from "../../graphql/Queries";
+import { useQuery } from "@apollo/client";
+import Loader from "./../shared/Loader";
 
 function BlogPage() {
+  const { slug } = useParams();
+
+  const { loading, data, error } = useQuery(GET_POST_INFO, {
+    variables: {
+      slug,
+    },
+  });
+
+  if (loading) return <Loader />;
+  if (error) return <h4>somethinf went wrong</h4>;
+
+  console.log(data);
+
   return <div>BlogPage</div>;
 }
 
--- graphql-materialul/src/graphql/Queries.js
+++ graphql-materialul/src/graphql/Queries.js
@@ -55,4 +55,25 @@ const GET_AUTHORS_INFO = gql`
   }
 `;
 
-export { GET_BLOGS_INFO, GET_AUTHORS_INFO, GET_AUTHOR_INFO };
+const GET_POST_INFO = gql`
+  query getPost($slug: String!) {
+    post(where: { slug: $slug }) {
+      author {
+        avatar {
+          url
+        }
+        name
+        field
+      }
+      content {
+        html
+      }
+      title
+      coverPhoto {
+        url
+      }
+    }
+  }
+`;
+
+export { GET_BLOGS_INFO, GET_AUTHORS_INFO, GET_AUTHOR_INFO, GET_POST_INFO };

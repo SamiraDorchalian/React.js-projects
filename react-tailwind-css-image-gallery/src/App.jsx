9af4341c5018dda81a90e9e1d7bc16dafaeea87a
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-22 20:28:34 +0330

    create IamgeSearch & loading

--- react-tailwind-css-image-gallery/src/App.jsx
+++ react-tailwind-css-image-gallery/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import ImagesCard from "./components/ImagesCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -17,15 +18,27 @@ function App() {
         setIsLoading(false);
       })
       .catch((error) => console.log(error));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <ImagesCard key={image.id} image={image} />
-        ))}
-      </div>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto mt-32">
+          No Images Found ...
+        </h1>
+      )}
+
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading ...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImagesCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- react-tailwind-css-image-gallery/src/components/ImagesCard.jsx
+++ react-tailwind-css-image-gallery/src/components/ImagesCard.jsx
@@ -25,9 +25,9 @@ function ImagesCard({ image }) {
         </ul>
       </div>
       <div className="px-6 py-4">
-        {tage.map((tag) => (
+        {tage.map((tag, index) => (
           <span
-            key={image.id}
+            key={index}
             className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
           >
             #{tag}

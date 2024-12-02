57c55ad331a4090b4d188b6448011ec10aac7cbd
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-02 11:35:17 +0330

    fetchData

---
+++ pagination-react/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+  );
+}
+
+export default App;

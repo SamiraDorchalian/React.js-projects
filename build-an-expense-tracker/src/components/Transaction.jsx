12d416615f304b8e67a56345b7d08e0dfa05a5dd
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-05 13:54:20 +0330

    create our context-Composition with reduce & create transactionList

---
+++ build-an-expense-tracker/src/components/Transaction.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  return (
+    <li
+      className={transaction.amount < 0 ? "minus" : "plus"}
+      key={transaction.id}
+    >
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+};

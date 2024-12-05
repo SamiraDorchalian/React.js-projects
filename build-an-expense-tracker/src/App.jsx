dd4515cd9590b1d0c490a58a9ebd3817eb752ca0
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-05 12:42:16 +0330

    The main structure

---
+++ build-an-expense-tracker/src/App.jsx
@@ -0,0 +1,23 @@
+import { Balance } from "./components/Balance";
+import { Header } from "./components/Header";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ build-an-expense-tracker/src/components/Balance.jsx
@@ -0,0 +1,8 @@
+export const Balance = () => {
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>$0.00</h1>
+    </>
+  );
+};

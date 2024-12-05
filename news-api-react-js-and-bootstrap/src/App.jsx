ce093d116990630f20ce8a754f58c3a91f24d7cf
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-05 15:57:03 +0330

    create Primary structure & fetchData

---
+++ news-api-react-js-and-bootstrap/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+import NewsBoard from "./Components/NewsBoard";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <NewsBoard />
+    </div>
+  );
+};
+
+export default App;
---
+++ news-api-react-js-and-bootstrap/src/Components/NewsBoard.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NewsBoard;

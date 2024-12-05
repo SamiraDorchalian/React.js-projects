59a8c2be0129fdd3fa9759eef5b4ee671cc268b3
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-05 14:46:23 +0330

    create another action the name of is addtransaction and contacted to fun alse uses in addTransaction components

--- build-an-expense-tracker/src/context/GlobalState.jsx
+++ build-an-expense-tracker/src/context/GlobalState.jsx
@@ -2,12 +2,7 @@ import { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 export const GlobalContext = createContext(initialState);
@@ -22,11 +17,19 @@ export const GlobalProvider = ({ children }) => {
     });
   }
 
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
         deleteTransaction,
+        addTransaction,
       }}
     >
       {children}

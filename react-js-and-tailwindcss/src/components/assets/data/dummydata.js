de73ec9bfc069aae1e22421b71420ec07843c3e2
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-10 16:11:12 +0330

    create footer and Data for footer

--- react-js-and-tailwindcss/src/components/assets/data/dummydata.js
+++ react-js-and-tailwindcss/src/components/assets/data/dummydata.js
@@ -38,3 +38,76 @@ export const courses = [
     title: "The business Intellgence analyst Course 2022",
   },
 ];
+
+export const footerData = [
+  {
+    id: 1,
+    header: "Company",
+    subtitle: [
+      {
+        id: 11,
+        header: "Contact",
+      },
+      {
+        id: 12,
+        header: "Protfolio",
+      },
+      {
+        id: 13,
+        header: "Blog",
+      },
+      {
+        id: 14,
+        header: "Our Team",
+      },
+      {
+        id: 15,
+        header: "Get In Touch",
+      },
+      {
+        id: 16,
+        header: "FAQ",
+      },
+    ],
+  },
+  {
+    id: 2,
+    header: "Platform",
+    subtitle: [
+      {
+        id: 11,
+        header: "Pricing",
+      },
+      {
+        id: 12,
+        header: "Blog",
+      },
+      {
+        id: 13,
+        header: "Landing",
+      },
+    ],
+  },
+  {
+    id: 3,
+    header: "Subscribe",
+    subtitle: [
+      {
+        id: 11,
+        header: "Contact",
+      },
+      {
+        id: 12,
+        header: "About Us",
+      },
+      {
+        id: 13,
+        header: "Reviews",
+      },
+      {
+        id: 14,
+        header: "Services",
+      },
+    ],
+  },
+];
--- react-js-and-tailwindcss/src/components/common/Footer.jsx
+++ react-js-and-tailwindcss/src/components/common/Footer.jsx
@@ -1,7 +1,9 @@
 import { BsApple, BsGooglePlay } from "react-icons/bs";
+import { footerData } from "../assets/data/dummydata";
+import { NavLink } from "react-router-dom";
 const Footer = () => {
   return (
-    <div>
+    <>
       <section className="app w-4/5 m-auto rounded-lg shadow-shadow2 text-white flex md:flex-col bg-primary mt-16 relative z-10">
         <div className="left w-[60%] md:w-full p-10">
           <h1 className="text-4xl font-semibold leading-tight">
@@ -24,7 +26,32 @@ const Footer = () => {
           </div>
         </div>
       </section>
-    </div>
+
+      <footer className="bg-[#f3f4f8] py-10 pt-32 -mt-24">
+        <div className="container grid grid-cols-4 gap-5 md:grid-cols-2">
+          <div className="logo">
+            <img src="" alt="imagelogo" className="h5" />
+            <span className="text-[14px]">
+              Great lesson ideas and lesson plans for ESL teachers! Educators
+              can customize lesson plans to best.
+            </span>
+          </div>
+
+          {footerData.map((val, index) => (
+            <li key={index}>
+              <h4 className="text-black text-sm font-semibold mb-5">
+                {val.header}
+              </h4>
+              {val.subtitle.map((sub, index) => (
+                <NavLink key={index} className=" block mb-2">
+                  {sub.header}
+                </NavLink>
+              ))}
+            </li>
+          ))}
+        </div>
+      </footer>
+    </>
   );
 };
 

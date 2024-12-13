090c4e046101e5cd719c7502480d0c2d2a7fb10d
samira dorchalian <samiradorchalain1998@gmail.com>
2024-12-13 19:58:19 +0330

    Creating the initial structure of the desired folders and files

---
+++ hotel-system-using-react-js/src/components/common/Sociallcons.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Sociallcons() {
+  return (
+    <div>Sociallcons</div>
+  )
+}
---
+++ hotel-system-using-react-js/src/components/data/Data.jsx
@@ -0,0 +1,340 @@
+export const navList = [
+  {
+    id: 1,
+    path: "/",
+    text: "Home",
+  },
+  {
+    id: 2,
+    path: "/about",
+    text: "About",
+  },
+  {
+    id: 3,
+    path: "/services",
+    text: "Services",
+  },
+  {
+    id: 4,
+    path: "/rooms",
+    text: "Rooms",
+  },
+  {
+    id: 5,
+    path: "/page",
+    text: "Page",
+    subItems: [
+      {
+        id: 51,
+        path: "/booking",
+        text: "Booking",
+      },
+      {
+        id: 52,
+        path: "/team",
+        text: "Our Team",
+      },
+      {
+        id: 53,
+        path: "/testimonial",
+        text: "Testimonial",
+      },
+    ],
+  },
+  {
+    id: 6,
+    path: "/contact",
+    text: "Contact",
+  },
+];
+
+export const socialIcons = [
+  {
+    icon: <i className="fab fa-facebook-f"></i>,
+  },
+  {
+    icon: <i className="fab fa-twitter"></i>,
+  },
+  {
+    icon: <i className="fab fa-instagram"></i>,
+  },
+  {
+    icon: <i className="fab fa-linkedin-in"></i>,
+  },
+  {
+    icon: <i className="fab fa-youtube"></i>,
+  },
+];
+
+export const carouselData = [
+  {
+    img: "",
+    title: "Discover A Brand Luxurious Hotel",
+    subtitle: "luxury living",
+    btn1: "Our Room",
+    btn2: "Book Room",
+  },
+  {
+    img: "",
+    title: "Discover A Brand Luxurious Hotel",
+    subtitle: "luxury living",
+    btn1: "Our Room",
+    btn2: "Book Room",
+  },
+];
+
+export const about = [
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary mb-2"></i>,
+    text: "Rooms",
+    count: "7861",
+  },
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary mb-2"></i>,
+    text: "Staffs",
+    count: "1234",
+  },
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary mb-2"></i>,
+    text: "Clients",
+    count: "4321",
+  },
+];
+
+export const services = [
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary"></i>,
+    name: "Room & Appartment",
+    discription: "Contrary to popular belief, ipsum is not simply random",
+  },
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary"></i>,
+    name: "Food & Restaurant",
+    discription: "Contrary to popular belief, ipsum is not simply random",
+  },
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary"></i>,
+    name: "Spa $ Fitness",
+    discription: "Contrary to popular belief, ipsum is not simply random",
+  },
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary"></i>,
+    name: "Sports $ Gaming",
+    discription: "Contrary to popular belief, ipsum is not simply random",
+  },
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary"></i>,
+    name: "Event $ Party",
+    discription: "Contrary to popular belief, ipsum is not simply random",
+  },
+  {
+    icon: <i className="fa fa-hotel fa-2x text-primary"></i>,
+    name: "GYM $ Yoga",
+    discription: "Contrary to popular belief, ipsum is not simply random",
+  },
+];
+
+export const team = [
+  {
+    image: "",
+    name: "Full Name",
+    designation: "Designation",
+  },
+  {
+    image: "",
+    name: "Full Name",
+    designation: "Designation",
+  },
+  {
+    image: "",
+    name: "Full Name",
+    designation: "Designation",
+  },
+  {
+    image: "",
+    name: "Full Name",
+    designation: "Designation",
+  },
+  {
+    image: "",
+    name: "Full Name",
+    designation: "Designation",
+  },
+];
+
+export const FooterItem = [
+  {
+    id: 1,
+    header: "Company",
+    UnitItem: [
+      {
+        name: "About Us",
+      },
+      {
+        name: "Contact Us",
+      },
+      {
+        name: "Privacy Policy",
+      },
+      {
+        name: "Terms & Condition",
+      },
+      {
+        name: "Support",
+      },
+    ],
+  },
+  {
+    id: 1,
+    header: "Services",
+    UnitItem: [
+      {
+        name: "Food & Restaurant",
+      },
+      {
+        name: "Spa & Fitness",
+      },
+      {
+        name: "Sports & Gaming",
+      },
+      {
+        name: "Event & Party",
+      },
+      {
+        name: "GYM & Yoga",
+      },
+    ],
+  },
+];
+
+export const footerContact = [
+  {
+    icon: <i className="fa fa-map-marker-alt me-3"></i>,
+    name: "123 Street, New York, USA",
+  },
+  {
+    icon: <i className="fa fa-phone-alt me-3"></i>,
+    name: "+023 345 67890",
+  },
+  {
+    icon: <i className="fa fa-envelope me-3"></i>,
+    name: "info@example.com",
+  },
+];
+
+export const contact = [
+  {
+    icon: <i className="fa fa-envelope-open text-primary me-2"></i>,
+    title: "Booking",
+    email: "book@example.com",
+  },
+  {
+    icon: <i className="fa fa-envelope-open text-primary me-2"></i>,
+    title: "Technical",
+    email: "tech@example.com",
+  },
+  {
+    icon: <i className="fa fa-envelope-open text-primary me-2"></i>,
+    title: "General",
+    email: "info@example.com",
+  },
+];
+
+export const testimonial = [
+  {
+    designation:
+      "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+    profession: "Profession",
+    icon: (
+      <i className="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4"></i>
+    ),
+    img: "",
+  },
+  {
+    designation:
+      "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+    profession: "Profession",
+    icon: (
+      <i className="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4"></i>
+    ),
+    img: "",
+  },
+  {
+    designation:
+      "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+    profession: "Profession",
+    icon: (
+      <i className="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4"></i>
+    ),
+    img: "",
+  },
+];
+
+export const roomItem = [
+  {
+    img: "",
+    price: "$110/night",
+    name: "Junior Suit",
+    star: [
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+    ],
+    designation:
+      "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+    yellowbtn: "View Detail",
+    darkbtn: "book now",
+  },
+  {
+    img: "",
+    price: "$110/night",
+    name: "Executive Suite",
+    star: [
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+    ],
+    designation:
+      "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+    yellowbtn: "View Detail",
+    darkbtn: "book now",
+  },
+  {
+    img: "",
+    price: "$110/night",
+    name: "Super Deluxe",
+    star: [
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+      <small class="fa fa-star text-orimary"></small>,
+    ],
+    designation:
+      "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+    yellowbtn: "View Detail",
+    darkbtn: "book now",
+  },
+];
+
+export const Facility = [
+  {
+    icon: <i className="fa fa-bed text-primaty me-2"></i>,
+    quantity: 3,
+    facility: "bed",
+  },
+  {
+    icon: <i className="fa fa-bath text-primaty me-2"></i>,
+    quantity: 3,
+    facility: "bath",
+  },
+  {
+    icon: <i className="fa fa-wifi text-primaty me-2"></i>,
+    quantity: 3,
+    facility: "wifi",
+  },
+];
